359b63824a93eda32cbfad5a6f42c6b9d5ed660f
Becca Ricks <baricks@gmail.com>
2017-02-20 00:14:16 -0500

    first commit

---
+++ facebook.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import threading
+import shutil
+import requests
+
+currentLoop = 0
+
+def downloadImage():
+
+    # Create unique filenames
+    global currentLoop
+    currentLoop += 1
+    print "Current loop is " + str(currentLoop)
+    image_name = str(currentLoop) + ".png"
+
+    # # Am I the only one tagged?
+    # tagged = driver.find_element_by_class_name('tagswrapper')
+    # print tagged
+
+    # Get the image src
+    big_image = driver.find_element_by_class_name('spotlight')
+    img_url = big_image.get_attribute("src")
+    response = requests.get(img_url, stream=True)
+
+    # Download the image
+    with open(image_name, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    del response
+    time.sleep(2)
+
+    # Click to the next image
+    big_image = driver.find_element_by_class_name('spotlight')
+    big_image.click()
+    time.sleep(2)
+
+    # Start another loop
+    threading.Timer(3, downloadImage()).start()
+
+
+# Set up Driver
+driver = webdriver.PhantomJS()
+driver.get("http://www.facebook.com/login")
+
+# Log into Facebook
+login = driver.find_element_by_css_selector('#email')
+login.send_keys(' ')
+password = driver.find_element_by_css_selector('#pass')
+password.send_keys(' ')
+password.send_keys(Keys.RETURN)
+time.sleep(2)
+
+# Go to tagged photos
+username = ' '
+url = "https://www.facebook.com/" + username + "/photos_of"
+driver.get(url)
+time.sleep(2)
+
+# Click on the first tagged photo
+gallery_image = driver.find_elements_by_class_name('uiMediaThumbImg')
+gallery_image[0].click()
+
+# Run the download image function
+downloadImage()
+
+driver.quit()
---
+++ linkedin.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import urllib2
+import time
+import shutil
+import requests
+
+driver = webdriver.PhantomJS()
+driver.get("https://www.linkedin.com/uas/login")
+
+# Log into LinkedIn
+login = driver.find_element_by_css_selector('#session_key-login')
+login.send_keys(' ')
+password = driver.find_element_by_css_selector('#session_password-login')
+password.send_keys(' ')
+password.send_keys(Keys.RETURN)
+time.sleep(3)
+
+# Go to list of connections
+driver.get("https://www.linkedin.com/mynetwork/invite-connect/connections/")
+time.sleep(3)
+
+
+# Cycle through each connection's picture
+for i in range(0,600):
+
+    print "On loop #" + str(i)
+
+    profile = driver.find_elements_by_class_name('lazy-image')
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    profile[i].click()
+    time.sleep(3)
+
+    # Get profile picture url
+    profile_pic = driver.find_element_by_class_name('pv-top-card-section__image')
+    img_url = profile_pic.get_attribute("src")
+    response = requests.get(img_url, stream=True)
+    image_name = str(i) + ".png"
+
+    # Download profile picture
+    with open(image_name, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    del response
+
+    driver.back()
+    time.sleep(3)
+
+    # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # time.sleep(3)
+
+driver.quit()
